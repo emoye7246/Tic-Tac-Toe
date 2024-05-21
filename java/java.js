add227b764547e54b13130c0aab9fec7152630af
Elijah Moye <Elijahmoye96@gmail.com>
2024-05-21 01:19:50 -0400

    JUST PLAYIN AROUND
    I Have no idea what
    im doin

---
+++ java/java.js
@@ -0,0 +1,11 @@
+(function (){
+    console.log("hello");
+
+    let gameBoard = function(places, winner) {
+        
+        let Gameboard = [];
+
+        Gameboard.places = places;
+        Gameboard.winner = winner;
+    }
+} )();
